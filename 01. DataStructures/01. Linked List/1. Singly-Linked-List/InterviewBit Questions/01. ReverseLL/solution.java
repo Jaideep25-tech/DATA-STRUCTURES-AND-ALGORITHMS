f36f3b9c8eecb960f685f16708ea6943bb6260a6
Jaideep Singh <79747022+Jaideep25-tech@users.noreply.github.com>
2021-09-03 09:18:33 -0700

    Update solution.java

--- 01. DataStructures/01. Linked List/1. Singly-Linked-List/InterviewBit Questions/01. ReverseLL/solution.java
+++ 01. DataStructures/01. Linked List/1. Singly-Linked-List/InterviewBit Questions/01. ReverseLL/solution.java
@@ -1,3 +1,11 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     ListNode(int x) { val = x; next = null; }
+ * }
+ */
 public class Solution {
     public ListNode reverseList(ListNode A) {
         ListNode curr=A;
