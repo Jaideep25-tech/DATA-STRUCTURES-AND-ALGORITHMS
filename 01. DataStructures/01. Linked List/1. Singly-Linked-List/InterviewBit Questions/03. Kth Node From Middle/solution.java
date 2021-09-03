24cc3477fcdcbd9ee1794c2aadbb64171ba4e5d5
Jaideep Singh <79747022+Jaideep25-tech@users.noreply.github.com>
2021-09-03 09:16:59 -0700

    Create solution.java

---
+++ 01. DataStructures/01. Linked List/1. Singly-Linked-List/InterviewBit Questions/03. Kth Node From Middle/solution.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public int solve(ListNode A, int B) {
+        ListNode t=A;int n=0;
+        while(t!=null){
+            n++;
+            t=t.next;
+        }
+        int mn=(n/2)+1;
+        t=A;
+        int nd=mn-B;
+        if(nd<1){return -1;}
+        else{
+        for(int i=1;i<nd;i++)
+        {
+            t=t.next;
+        }
+        return t.val;}
+    }
+}
